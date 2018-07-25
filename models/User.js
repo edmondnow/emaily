786ee4aea56a250d12a11cef4b62950a8e39a324
unknown <edmond.bitay@gmail.com>
2018-07-25 03:06:26 +0200

    addded

---
+++ models/User.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  googleId: { type: String, unique: true }
+});
+
+mongoose.model('users', userSchema);
---
+++ services/passport.js
@@ -0,0 +1,39 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys.js');
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  //user.id stands for mongo generated id
+  //use this id, since we don't know which oauth strategy was used (fb or google etc.)
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      //the route where our user is going to be sent after they grant permissions to our app
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
+    }
+  )
+);
